8af63f93e3bfe7caa8e60c1b2a33cff1a0c465ff
Aspallar <shadowthrone99@outlook.com>
2020-08-25 21:52:24 +0100

    Removed unnecessary usings

--- src/FalkirkBinAlert/SettingsWindow.xaml.cs
+++ src/FalkirkBinAlert/SettingsWindow.xaml.cs
@@ -1,23 +1,11 @@
 ﻿using AngleSharp.Dom;
 using AngleSharp.Html.Parser;
 using MahApps.Metro.Controls;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
 using System.Net;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Web;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace FalkirkBinAlert
 {
